a5fcd388bc218386f8399b3ff525700d6035a33b
Bogdan Bilbie <bogdan.bilbie@gmail.com>
2020-06-09 09:09:00 +0300

    few corrections

--- PHP/Statistics(L).php
+++ PHP/Statistics(L).php
@@ -56,9 +56,13 @@
 
 
 <div class = "statistici" >
+
   <?php
+  $arrayFS=$arrayCS=$arrayUC=$arrayFAS=$arrayFA=0;
+  
   $sql = "SELECT s.name as 'sname', a.name as 'aname' , count(*) as 'c' from favorite_songs fa join songs s on s.id = fa.song_id join artists a on s.artist_id = a.id GROUP by song_id order by c desc";
   if($result = mysqli_query($db,$sql)){
+    if(mysqli_num_rows($result) > 0){
     $row = mysqli_fetch_array($result);
     $nr_aparitii = $row['c'];
     $song = $row['sname'];
@@ -66,11 +70,13 @@
     ?>
     <p style="font-size: 30px;"> The most favorite song is: <?php echo $song?><p> Singed by: <?php echo $artist ?><br> It is favorited by: <?php echo $nr_aparitii ?> people.</p>
     <?php
-  }
     $arrayFS = array($song,$artist,$nr_aparitii);
+    }
+}
 
   $sql = "SELECT s.name as 'song', a.name as 'aname', count(*) as 'c' from comments c join songs s on s.id = c.song_id join artists a on s.artist_id = a.id group by song_id order by c desc";
   if($result = mysqli_query($db,$sql)){
+    if(mysqli_num_rows($result) > 0){
     $row = mysqli_fetch_array($result);
     $nr_aparitii = $row['c'];
     $song = $row['song'];
@@ -78,41 +84,49 @@
     ?>
     <p style="font-size: 30px;"> The most commented song is: <?php echo $song ?><p>Singed by:<?php echo $artist ?><br> It has: <?php echo $nr_aparitii ?> comments.</p>
     <?php
+    $arrayCS = array($song, $artist,$nr_aparitii);
+    }
   }
-  $arrayCS = array($song, $artist,$nr_aparitii);
 
   $sql = "SELECT user_name as 'user', count(id) as 'c' from comments group by user_name order by c desc ";
   if($result = mysqli_query($db,$sql)){
+    if(mysqli_num_rows($result)>0){
     $row = mysqli_fetch_array($result);
     $nr_aparitii = $row['c'];
     $user = $row['user'];
     ?>
     <p style="font-size: 30px;">The most active user is <?php echo $user ?><p>He/She added: <?php echo $nr_aparitii ?> comments.</p>
     <?php
+    $arrayUC = array($user,$nr_aparitii);
+    }
   }
-  $arrayUC = array($user,$nr_aparitii);
 
   $sql = "SELECT fs.id, a.name as 'aname', count(a.id) as 'c' from favorite_songs fs join songs s on s.id = fs.song_id join artists a on a.id = s.artist_id group by a.id order by c desc";
   if($result = mysqli_query($db,$sql)){
+    if(mysqli_num_rows($result)>0){
     $row = mysqli_fetch_array($result);
     $nr_aparitii = $row['c'];
     $artist = $row['aname'];
     ?>
     <p style="font-size: 30px;"> The artist that has the most songs added to favorite by users is: <?php echo $artist ?><p> Songs added at favorite: <?php echo $nr_aparitii?> times.</p>
     <?php
+    $arrayFAS = array($artist,$nr_aparitii);
+    }
   }
-  $arrayFAS = array($artist,$nr_aparitii);
 
   $sql = " SELECT a.name as 'aname', count(*) as 'c' from favorite_artists fa join artists a on fa.artist_id = a.id group by fa.artist_id order by c desc";
   if($result = mysqli_query($db,$sql)){
+    if(mysqli_num_rows($result)>0){
     $row = mysqli_fetch_array($result);
     $nr_aparitii = $row['c'];
     $artist = $row['aname'];
     ?>
     <p style="font-size: 30px;"> The most favorited artist is <?php echo $artist?><p> Added as favorite by: <?php echo $nr_aparitii ?> users.</p>
     <?php
+    $arrayFA = array($artist,$nr_aparitii);
+
+    }
   }
-  $arrayFA = array($artist,$nr_aparitii);
 
 
 // pentru export
